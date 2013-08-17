2c52581a99c8d5918ed79d21d690d36f28e22d80
Calvin Lai <callai@gmail.com>
2013-08-17 19:48:34 -0400

    version 1.0.0

---
+++ src/ios/MWPhotoBrowserCordova.h
@@ -0,0 +1,26 @@
+//
+//  ImageViewer.h
+//  Helper
+//
+//  Created by Calvin Lai on 7/11/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+#import "MWPhotoBrowserCordova.h"
+
+@interface MWPhotoBrowserCordova : CDVPlugin <MWPhotoBrowserDelegate> {
+
+    NSMutableDictionary* callbackIds;
+    NSArray* photos;
+
+}
+
+@property (nonatomic, retain) NSMutableDictionary* callbackIds;
+@property (nonatomic, retain) NSArray *photos;
+
+- (void)showGallery:(NSArray *)images;
+
+
+@end
